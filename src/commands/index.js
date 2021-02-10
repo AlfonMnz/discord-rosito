a83ac0ea8e00f246b240c7b8752937bc81fb30f0
Alfonso <alfonmusan14@gmail.com>
2021-02-10 22:40:08 +0100

    Add new command

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,14 +1,17 @@
 import makeAddChannelToAlert from "./addChannelToAlert";
-
+import makeSayYourCreator from "./sayYourCreator";
 import {alertDb} from "../db";
 
 const addChannelToAlert = makeAddChannelToAlert(alertDb);
-
+const sayYourCreator = makeSayYourCreator();
 function runCommands(client) {
 	client.on('message', async (msg) => {
 		if (msg.content.startsWith('!aviso')) {
 			await addChannelToAlert(msg);
 			msg.channel.send('Se ha configurado este canal para avisar de la isla de las tentaciones');
+		}
+		if (msg.content.startsWith('!creador')) {
+			await sayYourCreator(msg);
 		}
 	})
 }
---
+++ src/commands/sayYourCreator.js
@@ -0,0 +1,5 @@
+export default function makeSayYourCreator() {
+	return async function sayYourCreator(msg) {
+		msg.channel.send('Después de Pablo y el fuego purificador, fui rescatado en forma de bot por @poliedro')
+	}
+}
