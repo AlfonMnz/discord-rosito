465249e677893b5c469fe914616520329a506117
Poliedro <alfonmusan14@gmail.com>
2021-03-11 21:26:54 +0100

    Adding notice 5 minutes

--- src/behaviour/intervals/alertTheIsland.js
+++ src/behaviour/intervals/alertTheIsland.js
@@ -1,8 +1,19 @@
 export default function makeAlertIslandBH(alertDb) {
-	return async function alertIslandBH(client) {
-		let ChannelToAlerts = await alertDb.getAll();
-		for (let channel of ChannelToAlerts) {
-			client.channels.cache.get(channel.ChannelId).send(channel.Message)
-		}
-	}
+
+    async function alertIslandBH(client) {
+        let ChannelToAlerts = await alertDb.getAll();
+        for (let channel of ChannelToAlerts) {
+            client.channels.cache.get(channel.ChannelId).send(channel.Message)
+        }
+    }
+
+    async function alertIsland5MBH(client) {
+        let ChannelToAlerts = await alertDb.getAll();
+        for (let channel of ChannelToAlerts) {
+            client.channels.cache.get(channel.ChannelId).send(`@everyone Faltan 5 minutos, la isla está que prende 🔥🔥🔥🔥`)
+        }
+    }
+
+    return {alertIslandBH, alertIsland5MBH}
+
 }
--- src/behaviour/intervals/index.js
+++ src/behaviour/intervals/index.js
@@ -7,15 +7,18 @@ const alertIslandBH = makeAlertIslandBH(alertDb);
 const alertDebateBH = makeAlertDebateBH(alertDb);
 
 function runIntervals(client) {
-	setInterval(async () => {
-		let date = new Date();
-		if (date.getDay() === 4 && date.getHours() === 21 && date.getMinutes() === 30) {
-			await alertIslandBH(client)
-		}
-		if (date.getDay() === 1 && date.getHours() === 21 && date.getMinutes() === 30) {
-			await alertDebateBH(client)
-		}
-	}, 60000)
+    setInterval(async () => {
+        let date = new Date();
+        if (date.getDay() === 4 && date.getHours() === 21 && date.getMinutes() === 30) {
+            await alertIslandBH.alertIslandBH(client)
+        }
+        if (date.getDay() === 1 && date.getHours() === 21 && date.getMinutes() === 30) {
+            await alertDebateBH(client)
+        }
+        if (date.getDay() === 4 && date.getHours() === 21 && date.getMinutes() === 55) {
+            await alertIslandBH.alertIsland5MBH(client)
+        }
+    }, 60000)
 }
 
 export default runIntervals
